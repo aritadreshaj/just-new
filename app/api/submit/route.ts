585bf1666c6a123de76647ed3fff2d3e08cf76f1
aritadreshaj <aritadreshaj@outlook.com>
2025-05-11 23:14:14 +0200

    missing credentials

--- app/api/submit/route.ts
+++ app/api/submit/route.ts
@@ -23,8 +23,8 @@ const transporter = nodemailer.createTransport({
   port: 465,
   secure: true,
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
+    user: process.env.ZOHO_DOMAIN,
+    pass: process.env.ZOHO_PASSWORD,
   },
 });
 
